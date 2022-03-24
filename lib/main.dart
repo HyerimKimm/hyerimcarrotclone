1a3571fd83308aeb806f91d4755e56c00d020de4
HyerimKimm <helim01033@naver.com>
2022-03-25 02:07:08 +0900

    1. 스플래쉬 -> 메인화면으로 1초 후 이동, 이동 시 500ms간 애니메이션 추가 2. logger 한 번 추가해봄.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:hyerimcarrotclone/splash_screen.dart';
+import 'package:hyerimcarrotclone/logger.dart';
 
 void main(){
+  logger.d('My First Log by logger!');
   runApp(MyApp());
 }
 
@@ -9,18 +11,22 @@ class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context){
     return FutureBuilder(
-      future: Future.delayed(Duration(seconds: 3), ()=>'100'),
+      future: Future.delayed(Duration(seconds: 1), ()=>'100'),
       builder: (context, snapshot) {
-        if(snapshot.hasError){
-          print('error occur while loading.');
-          return Text('Error occur');
-        }else if(snapshot.hasData){
-          return AbocadoApp();
-        }else{
-          return SplashScreen();
-        }
+        return AnimatedSwitcher(duration: Duration(milliseconds: 500), child: _splashLoadingWidget(snapshot));
       }
     );
+  }
+
+  StatelessWidget _splashLoadingWidget(AsyncSnapshot<Object?> snapshot) {
+    if(snapshot.hasError){
+      print('error occur while loading.');
+      return Text('Error occur');
+    }else if(snapshot.hasData){
+      return AbocadoApp();
+    }else{
+      return SplashScreen();
+    }
   }
 }
 
