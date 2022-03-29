435219ebd3cfa5a3643852cad1ca76d1b4759804
HyerimKimm <helim01033@naver.com>
2022-03-30 00:48:50 +0900

    intro page 추가

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:hyerimcarrotclone/screens/start/intro_page.dart';
 
 class AuthScreen extends StatelessWidget {
   const AuthScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text('Auth Screen!'));
+    return Scaffold(
+        body: PageView(children:[
+          IntroPage(),
+          Container(color: Colors.accents[2]),
+          Container(color: Colors.accents[5],)
+        ])
+    );
   }
 }
